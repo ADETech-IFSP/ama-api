c6aac34c75d1d2838842f77a88b9861d592357ff
Adryan Alencar <adryan.alencar@aluno.ifsp.edu.br>
2021-06-21 22:55:56 -0300

    Criado entidade User

---
+++ src/entity/UserModel.ts
@@ -0,0 +1,45 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("user")
+class User{
+
+    @PrimaryGeneratedColumn()
+    readonly id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    last_name: string;
+
+    @Column()
+    birth_date: Date;
+
+    @Column()
+    email: string;
+
+    @Column()
+    telephone: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    photo_url: string;
+
+    @Column()
+    gender: number;
+
+    @Column()
+    join_date: Date;
+
+    @Column()
+    confirm_code: string;
+
+    @Column()
+    verified: boolean;
+
+    @Column()
+    user_type: number;
+    
+}
