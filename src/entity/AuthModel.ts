4dcda3a8bf8fcb0b7a5d8bd9662127f49d0a4b7d
Miguel Carvalho <carvalho.miguel@aluno.ifsp.edu.br>
2021-06-21 22:58:07 -0300

    created entity auth

---
+++ src/entity/AuthModel.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn  } from "typeorm";
+
+@Entity("auth")
+class Auth {
+
+    @PrimaryColumn()    
+    readonly id : number;
+
+    @Column()
+    user_id : number;
+
+    @Column()
+    token : string;
+        
+    @Column()
+    created_date : Date;
+
+    @Column()
+    expira_date : Date; 
+
+}
