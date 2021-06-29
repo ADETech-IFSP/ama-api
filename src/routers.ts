9a35dc5de9444d075ea28d7ffa4d0536774bed8d
Adryan Alencar <adryan.alencar@aluno.ifsp.edu.br>
2021-06-29 01:00:55 -0300

    Read e Delete para Users

--- src/routers.ts
+++ src/routers.ts
@@ -5,5 +5,7 @@ const router = Router();
 const userController = new UserController();
 
 router.post('/user/create', userController.create);
+router.get('/user/delete/:id', userController.delete);
+router.get('/user/:id', userController.read);
 
 export { router }
