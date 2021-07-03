7a46e9a36baa47219bb33991729ffe0b2eaa04a6
Breno Ferreira <breno.g@aluno.ifsp.edu.br>
2021-07-02 23:34:20 -0300

    criação não terminada a respeito do read, update e delete no PetController

--- src/controllers/PetController.ts
+++ src/controllers/PetController.ts
@@ -37,4 +37,39 @@ export class PetController{
 
         }).status(201)
     }
+
+    async read(request: Request, response: Response){
+        const{
+            id
+        } = request.params;
+
+        const petRepository = getCustomRepository(PetRepository);
+        const pet = await petRepository.findOne({
+            id: Number(id)
+        })
+
+        if(!pet){
+            return response.json({
+                status: "error",
+                message: "Pet não encontrado."
+            }).status(404);
+        }
+        
+        return response.json({
+            status: "success",
+            message: "Pet deletado com sucesso!",
+            pet
+        }).status(200);
+    }
+
+    async update(request: Request, response: Response){
+  
+    }
+
+    async delete(request: Request, response: Response){
+        const{
+            id
+        } = request.params;
+    }
+
 }
