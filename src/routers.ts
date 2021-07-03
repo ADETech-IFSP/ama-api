e8be94a749079cc039cb604288b597cd2f2838e7
Miguel Carvalho <carvalho.miguel@aluno.ifsp.edu.br>
2021-07-03 07:43:09 -0300

    nao terminado o update,read e delete

--- src/routers.ts
+++ src/routers.ts
@@ -1,18 +1,22 @@
 import { Router } from 'express';
-import { CategoryController, QuestionController, UserController  } from './controllers';
+import { CategoryController, QuestionController, UserController, } from './controllers';
 
 const router = Router();
 const userController = new UserController();
 const categoryController = new CategoryController(); 
 const questionController = new QuestionController();
 
+
 router.post('/user/create', userController.create);
 router.get('/user/delete/:id', userController.delete);
 router.get('/user/:id', userController.read);
+
 router.post('/forum/create', questionController.create);
 router.get('/forum/:id', questionController.read);
 
 
 router.post('/category/create',categoryController.create);
+
+
 
 export { router }
