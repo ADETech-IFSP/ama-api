297db3ab2ca9fa3449cb206d6afc0dd15e1d638a
Adryan Alencar <adryan.alencar@aluno.ifsp.edu.br>
2021-07-05 23:49:08 -0300

    Correção de CORS

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,11 @@ import createConnection from './database';
 import { router } from './routers';
 
 createConnection();
-const app = express();
+const app = express();  
 
 app.use((req, res, next) => {	
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", '*');
-    app.use(cors());
     next();
 });
 
