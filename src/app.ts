ba4afdafa79be602c7fe55821b0beee3bf92b2da
Adryan Alencar <adryan.alencar@aluno.ifsp.edu.br>
2021-08-01 18:16:01 -0300

    updated cors politcs

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express = require('express');
 import 'reflect-metadata';
 import createConnection from './database';
 import { router } from './routers';
+var cors = require('cors') 
 
 createConnection();
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(router);
 
